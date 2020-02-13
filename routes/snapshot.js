84316f0f1ddda4d807ba9440db167e443def8f33
Jesse Okeya <Jesseokeya@gmail.com>
2020-02-13 17:33:02 -0500

    Update snapshot.js

--- routes/snapshot.js
+++ routes/snapshot.js
@@ -6,10 +6,23 @@ class Snapshot {
 
     initialize() {
         this.router.get('/snapshots/', (req, res) => this.getSnapshots(req, res))
+        this.router.post('/snapshots/', (req, res) => this.createSnapshot(req, res))
     }
 
     getSnapshots(req, res) {
         try {
+            console.log(req.body.snapshot.children)
+            res.send({
+                msg: 'snapshots!'
+            })
+        } catch (err) {
+            throw err
+        }
+    }
+
+    createSnapshot(req, res) {
+        try {
+            console.log(req.body)
             res.send({
                 msg: 'snapshots!'
             })
