44f81577ecf067e12cdfbdf3a619ec9ac32e5a12
Jesse Okeya <Jesseokeya@gmail.com>
2020-02-21 21:37:44 -0500

    Update notification.js

--- services/notification.js
+++ services/notification.js
@@ -1,35 +1,15 @@
 const {
     WebClient
 } = require('@slack/web-api');
-const {
-    createEventAdapter
-} = require('@slack/events-api');
+// const {
+//     createEventAdapter
+// } = require('@slack/events-api');
 
 class NotificationService {
     constructor(options = {}) {
         this.options = options
         const token = process.env.SLACK_TOKEN;
         this.slack = new WebClient(token);
-        // Initialize using signing secret from environment variables
-        const {
-            createEventAdapter
-        } = require('@slack/events-api');
-        const slackEvents = createEventAdapter(token);
-        const port = process.env.PORT || 3000;
-
-        // Attach listeners to events by Slack Event "type". See: https://api.slack.com/events/message.im
-        slackEvents.on('message', (event) => {
-            console.log(`Received a message event: user ${event.user} in channel ${event.channel} says ${event.text}`);
-        });
-
-        // Handle errors (see `errorCodes` export)
-        slackEvents.on('error', console.error);
-
-        // Start a basic HTTP server
-        slackEvents.start(port).then(() => {
-            // Listening on path '/slack/events' by default
-            // console.log(`Slack erver running on port ${port}`);
-        });
     }
 
     async slackNotify(context) {
